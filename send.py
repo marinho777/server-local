3e9d7fa8fffba7e93fc5f523b79fdd9ca6f6e2d6
daniel marinho <136478655+marinho777@users.noreply.github.com>
2024-01-20 23:29:38 -0300

    Delete send.py

--- send.py
+++
@@ -1,9 +0,0 @@
-import socket
-s=socket.socket()
-ip="127.0.0.1".encode()
-port=int(input("port - "))
-s.connect((ip, port))
-
-first=s.recv(1024)
-print(first)
-s.send(input("message to send back - "))
